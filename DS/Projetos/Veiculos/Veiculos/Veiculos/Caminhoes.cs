54a7dfc3d5250afe08caa8241702dadbd4e5da01
SthephanySouza <deethes2@gmail.com>
2023-01-09 17:08:16 -0300

    Update
    Adicionando os últimos projetos de 2022.

---
+++ DS/Projetos/Veiculos/Veiculos/Veiculos/Caminhoes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos
+{
+    internal class Caminhoes : Veiculos
+    {
+        // variaveis privadas da classe Caminhoes, pertencentes apenas a ela
+        private double cargaMax;
+        private string tipoCarga;
+        private int velocMax;
+        private int numEixos;
+
+        // consulta sem parâmetros
+        public Caminhoes()
+        {
+            this.cargaMax = 0.0;
+            this.tipoCarga = null;
+            this.velocMax = 0;
+            this.numEixos = 0;
+        }
+
+        // consulta com parâmetros
+        public Caminhoes(string placa, double peso, double altura, double largura, string tipoCombu, string marca, double cargaMax, string tipoCarga, int velocMax, int numEixos)
+        {
+            this.Placa = placa;
+            this.Peso = peso;
+            this.Altura = altura;
+            this.Largura = largura;
+            this.TipoCombu = tipoCombu;
+            this.Marca = marca;
+            this.cargaMax = cargaMax;
+            this.tipoCarga = tipoCarga;
+            this.velocMax = velocMax;
+            this.numEixos = numEixos;
+        }
+
+        // propriedades dos atributos
+        private double CargaMax { get; set; }
+        private string TipoCarga { get; set; }
+        private int VelocMax { get; set; }
+        private int NumEixos { get; set; }
+
+        public override string Consultar()
+        {
+            String s = String.Empty;
+            s = $"Placa: {Placa}\n Peso: {Peso}\n Altura: {Altura}\n Largura: {Largura}\n Tipo Combustivel: {TipoCombu}\n Marca: {Marca}\n Carga máxima: {CargaMax}\n Tipo Carga: {TipoCarga}\n Velocidade máxima: {VelocMax}\n Número eixos: {numEixos}";
+            return s;
+        }
+    }
+}
---
+++ DS/Projetos/Veiculos/Veiculos/Veiculos/Veiculos/Veiculos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos
+{
+    internal class Veiculos
+    {
+        private string placa;
+        private double peso;
+        private double altura;
+        private double largura;
+        private string tipoCombu;
+        private string marca;
+
+        public Veiculos()
+        {
+            this.placa = null;
+            this.peso = 0.0;
+            this.altura = 0.0;
+            this.largura = 0.0;
+            this.tipoCombu = null;
+            this.marca = null;
+        }
+
+        public Veiculos(string placa, double peso, double altura, double largura, string tipoCombu, string marca)
+        {
+            this.placa = placa;
+            this.peso = peso;
+            this.altura = altura;
+            this.largura = largura;
+            this.tipoCombu = tipoCombu;
+            this.marca = marca;
+        }
+
+        public string Placa { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+        public string TipoCombu { get; set; }
+        public string Marca { get; set; }
+
+        public virtual string Consulta()
+        {
+            return $"Placa: {0}";
+        }
+    }
+}
